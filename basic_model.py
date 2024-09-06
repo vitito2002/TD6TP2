b2208edd5f01f794bf07b7fab52a83c4eea77c40
vitito2002 <vitinavajas@gmail.com>
2024-09-06 11:34:45 -0300

    frfr

--- basic_model.py
+++ basic_model.py
@@ -13,7 +13,7 @@ pd.set_option('display.width', None)
 train_data = pd.read_csv("ctr_21.csv")
 
 # Load the test data
-eval_data = pd.read_csv("ctr_test.csv")
+eval_data = pd.read_csv("ctr_test.csv") #hola  sapo sin cola
 
 # Train a tree on the train data
 train_data = train_data.sample(frac=1/10)
@@ -23,8 +23,35 @@ X_train = X_train.select_dtypes(include='number')
 del train_data
 gc.collect()
 
-cls = make_pipeline(SimpleImputer(), DecisionTreeClassifier(max_depth=8, random_state=2345))
-cls.fit(X_train, y_train)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+# Define the model
+rf = RandomForestClassifier(random_state=42)
+
+# Define the parameter grid
+param_grid = {
+	'n_estimators': [100, 200, 300],
+	'max_depth': [None, 10, 20, 30],
+	'min_samples_split': [2, 5, 10],
+	'min_samples_leaf': [1, 2, 4]
+}
+
+# Perform grid search
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+grid_search.fit(X_train, y_train)
+
+# Get the best model
+best_rf = grid_search.best_estimator_
+
+# Predict on the evaluation set
+eval_data = eval_data.select_dtypes(include='number')
+y_preds = best_rf.predict_proba(eval_data.drop(columns=["id"]))[:, 1]
+
+# Make the submission file
+submission_df = pd.DataFrame({"id": eval_data["id"], "Label": y_preds})
+submission_df["id"] = submission_df["id"].astype(int)
+submission_df.to_csv("basic_model.csv", sep=",", index=False)
 
 # Predict on the evaluation set
 eval_data = eval_data.select_dtypes(include='number')
@@ -34,3 +61,5 @@ y_preds = cls.predict_proba(eval_data.drop(columns=["id"]))[:, cls.classes_ == 1
 submission_df = pd.DataFrame({"id": eval_data["id"], "Label": y_preds})
 submission_df["id"] = submission_df["id"].astype(int)
 submission_df.to_csv("basic_model.csv", sep=",", index=False)
+
+
